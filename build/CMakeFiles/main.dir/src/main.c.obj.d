a7438232f5e0dc3c5c643c465f5bb3c6c12acb28
Greame <19705068801@139.com>
2025-05-04 02:19:12 +0800

    init commit

---
+++ build/CMakeFiles/main.dir/src/main.c.obj.d
@@ -0,0 +1,13 @@
+CMakeFiles/main.dir/src/main.c.obj: D:\Git\Zhang-Haozhong_hw1\src\main.c \
+ C:/PROGRA~2/Dev-Cpp/MinGW32/i686-w64-mingw32/include/stdio.h \
+ C:/PROGRA~2/Dev-Cpp/MinGW32/i686-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/PROGRA~2/Dev-Cpp/MinGW32/i686-w64-mingw32/include/corecrt.h \
+ C:/PROGRA~2/Dev-Cpp/MinGW32/i686-w64-mingw32/include/_mingw.h \
+ C:/PROGRA~2/Dev-Cpp/MinGW32/i686-w64-mingw32/include/_mingw_mac.h \
+ C:/PROGRA~2/Dev-Cpp/MinGW32/i686-w64-mingw32/include/_mingw_secapi.h \
+ C:/PROGRA~2/Dev-Cpp/MinGW32/i686-w64-mingw32/include/vadefs.h \
+ C:/PROGRA~2/Dev-Cpp/MinGW32/i686-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/PROGRA~2/Dev-Cpp/MinGW32/i686-w64-mingw32/include/_mingw_off_t.h \
+ C:/PROGRA~2/Dev-Cpp/MinGW32/i686-w64-mingw32/include/swprintf.inl \
+ C:/PROGRA~2/Dev-Cpp/MinGW32/i686-w64-mingw32/include/sec_api/stdio_s.h \
+ D:\Git\Zhang-Haozhong_hw1\src\algebra.h
